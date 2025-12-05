6b8ceff956794faddfa5d6d63d1d923908724270
Sriram Krishnaswamy <sriramkswamy@outlook.com>
2025-12-04 20:55:01 -0800

    Folder navigation and editing

--- lua/plugins/navigation.lua
+++ lua/plugins/navigation.lua
@@ -19,6 +19,21 @@ return {
     end
   },
 
+  { -- useful folder navigation/editing
+    'stevearc/oil.nvim',
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {},
+    -- Optional dependencies
+    -- dependencies = { { "nvim-mini/mini.icons", opts = {} } },
+    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+    lazy = false,
+    keys = {
+      { "<leader>-", "<cmd>Oil<CR>", desc = "File Navigation"}
+    }
+  },
+
   { -- jump to anything
     "folke/flash.nvim",
     event = "VeryLazy",
