79b2d71154d2bf4731385598aee516a448516d19
Sriram Krishnaswamy <sriramkswamy@outlook.com>
2025-12-06 15:13:25 -0800

    Better folding

--- lua/config/visual.lua
+++ lua/config/visual.lua
@@ -134,6 +134,17 @@ return {
         },
       })
     end
-  }
+  },
+
+  { -- auto-folding based on treesitter/lsp
+    "chrisgrieser/nvim-origami",
+    event = "VeryLazy",
+    opts = {}, -- needed even when using default config
+    -- recommended: disable vim's auto-folding
+    init = function()
+      vim.opt.foldlevel = 99
+      vim.opt.foldlevelstart = 99
+    end,
+  },
 
 }
