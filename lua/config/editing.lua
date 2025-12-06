a0dc6774843966677ff7441455de10d631b458db
Sriram Krishnaswamy <sriramkswamy@outlook.com>
2025-12-06 12:50:43 -0800

    Multicursor support and terminal map

--- lua/config/editing.lua
+++ lua/config/editing.lua
@@ -52,6 +52,48 @@ return {
     }
   },
 
+  { -- multiple cursors
+    "jake-stewart/multicursor.nvim",
+    branch = "1.0",
+    config = function()
+        local mc = require("multicursor-nvim")
+        mc.setup()
+        -- Mappings defined in a keymap layer only apply when there are
+        -- multiple cursors. This lets you have overlapping mappings.
+        mc.addKeymapLayer(function(layerSet)
+            -- Select a different cursor as the main one.
+            layerSet({"n", "x"}, "<C-b>", mc.prevCursor)
+            layerSet({"n", "x"}, "<C-f>", mc.nextCursor)
+            -- Delete the main cursor.
+            layerSet({"n", "x"}, "<C-d>", mc.deleteCursor)
+            -- Enable and clear cursors using escape.
+            layerSet("n", "<esc>", function()
+                if not mc.cursorsEnabled() then
+                    mc.enableCursors()
+                else
+                    mc.clearCursors()
+                end
+            end)
+        end)
+        -- Customize how cursors look.
+        local hl = vim.api.nvim_set_hl
+        hl(0, "MultiCursorCursor", { reverse = true })
+        hl(0, "MultiCursorVisual", { link = "Visual" })
+        hl(0, "MultiCursorSign", { link = "SignColumn"})
+        hl(0, "MultiCursorMatchPreview", { link = "Search" })
+        hl(0, "MultiCursorDisabledCursor", { reverse = true })
+        hl(0, "MultiCursorDisabledVisual", { link = "Visual" })
+        hl(0, "MultiCursorDisabledSign", { link = "SignColumn"})
+    end,
+    keys = {
+      { "<C-q>", mode = { "n", "x" }, function () require("multicursor-nvim").toggleCursor() end, desc = "Toggle MC" },
+      { "<C-n>", mode = { "n", "x" }, function () require("multicursor-nvim").matchAddCursor(1) end, desc = "Match MC Below" },
+      { "<C-e>", mode = { "n", "x" }, function () require("multicursor-nvim").matchSkipCursor(1) end, desc = "Skip Match MC Below" },
+      { "<C-p>", mode = { "n", "x" }, function () require("multicursor-nvim").matchAddCursor(-1) end, desc = "Match MC Above" },
+      { "<C-y>", mode = { "n", "x" }, function () require("multicursor-nvim").matchSkipCursor(-1) end, desc = "Skip Match MC Above" },
+    },
+  },
+
   { -- Custom variable segment text objects
     'Julian/vim-textobj-variable-segment',
     dependencies = {
